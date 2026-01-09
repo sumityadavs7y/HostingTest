ae6d7ede06fcff24e6f8c1db19625898e2bc7085
sumit yadav <sumit.yadav@devo.com>
2026-01-09 14:11:20 +0530

    branch commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello This is the master branch.\n TEST_ENV value is: ' + process.env.TEST_ENV);
+  res.end('Hello This is the some other branch.\n TEST_ENV value is: ' + process.env.TEST_ENV);
 });
 
 server.listen(port, hostname, () => {
